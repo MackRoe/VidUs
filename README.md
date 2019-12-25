15c4ccd758cd959af7f14a5cd9e733265705da6e
MackRoe <elaine.music@students.makeschool.com>
2019-12-25 07:57:02 -0800

    add README.md and requirements.txt

---
+++ README.md
@@ -0,0 +1,9 @@
+# VidUs
+
+following this
+[Tutorial](https://youtu.be/1XTqE7LFQjI)
+
+## Tech Stack
+--Python3
+--NumPy
+--OpenCV-Python
--- capture.py
+++ capture.py
@@ -4,5 +4,11 @@ import time
 # create object. zero for camera
 video = cv2.VideoCapture(0)
 
+# create a frame object
+check, frame = video.read()
+
+print(check)
+print(frame)  # represents image
+
 # shut down the camera
 video.release()
